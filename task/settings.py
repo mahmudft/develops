1d6b1ff609bf61eb76e63833d47811ce7b0df1ef
Mahmud <fatullayevm@gmail.com>
2022-02-20 01:29:01 +0400

    vvv

--- task/settings.py
+++ task/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('db', 'db'),
+        'HOST': os.environ.get('db'),
         'PORT': 5432,
     }
 }
